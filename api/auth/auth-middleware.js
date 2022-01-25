64281feb54c08132879ed7008fe4ba01fcfaad0d
wb1110 <strengthcoachwb@gmail.com>
2022-01-25 15:50:09 -0600

    fixed status code

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -46,7 +46,7 @@ const checkUsernameExists = async (req, res, next) => {
     try {
       const [user] = await Users.findBy({ username: req.body.username})
       if (!user) {
-        next({ status: 422, message: 'Invalid credentials' })
+        next({ status: 401, message: 'Invalid credentials' })
       } else {
         req.user = user
         next()
